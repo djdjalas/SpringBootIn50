d8f32973cac71e784641c2fafa5836157d5f768f
djdjalas <iamdjalas@gmail.com>
2016-12-07 12:40:21 +0000

    change service to be injected to mysql

--- src/main/java/com/yourname/service/StudentService.java
+++ src/main/java/com/yourname/service/StudentService.java
@@ -12,7 +12,7 @@ import java.util.Collection;
 public class StudentService {
 
     @Autowired
-    @Qualifier("mongoData")
+    @Qualifier("mysql")
     private StudentDao studentDao;
 
     public Collection<Student> getAllStudents(){
